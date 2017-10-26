fb23ab07b211ef4dfccf90f9d43a3ea79cba5257
ben-barker <ben.barker@domo.com>
2017-10-26 10:47:47 -0600

    finish overview

--- src/slides/02-Overview/index.js
+++ src/slides/02-Overview/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
-
 import {
   Slide,
   Heading,
   Image,
-  Layout,
-  Fill,
 } from 'spectacle';
+
+import flow from './flow.gif';
 
 export default [
   <Slide>
     <Heading size="2">So...what are they?</Heading>
+  </Slide>,
+  <Slide>
+    <Image src={ flow } width="100%" height="100%" />
+  </Slide>,
+  <Slide>
+    <Heading size="4">Selectors are the "reading API" for our state</Heading>
   </Slide>
 ];
