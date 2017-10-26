14e64aebf51bc3038a9b19ee1d0eb587a03f435b
ben-barker <ben.barker@domo.com>
2017-10-25 21:54:14 -0600

    refactor how assets are handled

--- src/slides/01-Intro/index.js
+++ src/slides/01-Intro/index.js
@@ -8,12 +8,14 @@ import {
   Fill,
 } from 'spectacle';
 
+import concerns from './separation-of-concerns.png';
+
 export default [
   <Slide>
     <Heading size="1">Selectors</Heading>
     <Heading size="4">...or how to separate concerns</Heading>
   </Slide>,
   <Slide>
-    <Image height="525px" width="525px" src="assets/images/separation-of-concerns.png" />
+    <Image height="525px" width="525px" src={ concerns } />
   </Slide>,
 ];
