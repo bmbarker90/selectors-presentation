d6ad0364c6be773115238a9f12be92f885f19b5d
ben-barker <ben.barker@domo.com>
2017-11-01 15:26:48 -0600

    add some examples

--- src/slides/06-Colocate/index.js
+++ src/slides/06-Colocate/index.js
@@ -5,6 +5,7 @@ import {
   Slide,
   Image,
   Heading,
+  CodePane,
 } from 'spectacle';
 
 import colocatingTweet from './colocating.png';
@@ -16,5 +17,13 @@ export default [
   <Slide>
     <Heading size="2">Proposal #3</Heading>
     <Heading size="4">Colocate selectors with reducers</Heading>
+  </Slide>,
+  <Slide>
+    <Heading size="4">store.js</Heading>
+    <CodePane lang="js" source={ require('!raw-loader!./examples/store.example') }></CodePane>
+  </Slide>,
+  <Slide>
+    <Heading size="4">selectors.js</Heading>
+    <CodePane lang="js" source={ require('!raw-loader!./examples/container.example') }></CodePane>
   </Slide>,
 ];
